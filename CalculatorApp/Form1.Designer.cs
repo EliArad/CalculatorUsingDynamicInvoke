3d72fe16d36ff2b1f2311ddec9a88a01ddd6216e
Eli Arad <easp13@gmail.com>
2022-11-06 15:04:17 +0200

    working code

---
+++ CalculatorApp/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace CalculatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFuncName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbDllName = new System.Windows.Forms.ComboBox();
+            this.txtClassName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFuncName
+            // 
+            this.txtFuncName.Location = new System.Drawing.Point(78, 135);
+            this.txtFuncName.Name = "txtFuncName";
+            this.txtFuncName.Size = new System.Drawing.Size(215, 20);
+            this.txtFuncName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Func Name:";
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(306, 133);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(75, 23);
+            this.btnExecute.TabIndex = 2;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Dll Names:";
+            // 
+            // cmbDllName
+            // 
+            this.cmbDllName.FormattingEnabled = true;
+            this.cmbDllName.Location = new System.Drawing.Point(76, 31);
+            this.cmbDllName.Name = "cmbDllName";
+            this.cmbDllName.Size = new System.Drawing.Size(209, 21);
+            this.cmbDllName.TabIndex = 5;
+            // 
+            // txtClassName
+            // 
+            this.txtClassName.Location = new System.Drawing.Point(76, 81);
+            this.txtClassName.Name = "txtClassName";
+            this.txtClassName.Size = new System.Drawing.Size(209, 20);
+            this.txtClassName.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Class Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtClassName);
+            this.Controls.Add(this.cmbDllName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFuncName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFuncName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbDllName;
+        private System.Windows.Forms.TextBox txtClassName;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CalculatorApp/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var files = Directory.EnumerateFiles(".", "SBF*.dll", SearchOption.TopDirectoryOnly)
+            .Where(s => s.EndsWith(".dll"));
+             
+            foreach (string s in files)
+            {
+                string s1 = s.Substring(2);
+                cmbDllName.Items.Add(s1);
+            }
+             
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            if (cmbDllName.Text == string.Empty)
+            {
+                MessageBox.Show("Please select DLL File name");
+                return;
+            }
+             
+            if (txtFuncName.Text == string.Empty)
+            {
+                MessageBox.Show("Please enter function name");
+                return;
+            }
+
+            if (txtClassName.Text == string.Empty)
+            {
+                MessageBox.Show("Please enter class name");
+                return;
+            }
+
+            string dllFile = cmbDllName.Text;
+            try
+            {
+                string curDir = Directory.GetCurrentDirectory();
+                string fullPathDllFile = curDir + "\\" + dllFile;
+                var assembly = Assembly.LoadFile(fullPathDllFile);
+                string functionType = $"SBF.{txtClassName.Text}";
+                var type = assembly.GetType(functionType);
+                if (type == null)
+                {
+                    MessageBox.Show("Failed to find function " + txtFuncName + " in namespace SBF file:" + dllFile);
+                }
+                var obj = Activator.CreateInstance(type);
+                var method = type.GetMethod(txtFuncName.Text);
+                object result = method.Invoke(obj, new object[] { 1, 2 });
+                string strResult = result.ToString();
+                MessageBox.Show(strResult);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+    }
+}
